35d5bfd6344c33ba4ddc55c75753e33700d0402b
techtee19 <techtee19@gmail.com>
2025-03-17 08:32:22 +0100

    weekend-budget

---
+++ script.js
@@ -0,0 +1,38 @@
+const budgetDisplay = document.querySelector(".budgetDisplay");
+const expenseDisplay = document.querySelector(".expenseDisplay");
+const remainingDisplay = document.querySelector(".remainingDisplay");
+const expenseInput = document.querySelector(".expenseInput");
+
+const budget = 1000;
+
+let expense = 0;
+
+const spending = {
+  budget: budget,
+  expense: expense,
+  getRemaining: function () {
+    return this.budget - this.expense;
+  },
+};
+
+function updateDisplay() {
+  budgetDisplay.textContent = `Budget: $${spending.budget}`;
+  expenseDisplay.textContent = `Spent: $${spending.expense}`;
+  remainingDisplay.textContent = `Remaining: $${spending.getRemaining()}`;
+}
+
+function addExpense() {
+  let inputField = expenseInput;
+  let newExpense = parseFloat(inputField.value);
+
+  if (newExpense > 0 && newExpense >= budget) {
+    spending.expense += newExpense;
+    updateDisplay();
+  } else {
+    alert("enter valid expense amount");
+  }
+
+  inputField.value = ""; // clears input field
+}
+
+updateDisplay();
